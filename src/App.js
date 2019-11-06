ec903c5ddee122909682205e683356947e127c64
ArvinthC3000 <54614142+ArvinthC3000@users.noreply.github.com>
2019-11-06 10:35:38 +0530

    Strict compare

--- src/App.js
+++ src/App.js
@@ -20,30 +20,30 @@ class App extends Component {
 
     if(this.gameState.gameEnded) return;
 
-    if(this.gameState.board[event.target.dataset.square] == ''){
+    if(this.gameState.board[event.target.dataset.square] === ''){
       this.gameState.board[event.target.dataset.square] = this.gameState.turn;
       event.target.innerText = this.gameState.turn;
 
-      this.gameState.turn = this.gameState.turn == 'X' ? 'O' : 'X';
+      this.gameState.turn = this.gameState.turn === 'X' ? 'O' : 'X';
        
       this.gameState.totalMoves++;
     }
     
     var result = this.checkWinner()
 
-    if(result == 'X'){
+    if(result === 'X'){
       this.gameState.gameEnded = true;
       this.setState({
         winner : 'X',
         winnerLine: 'Match won by X'
       })
-    } else if (result == 'O'){
+    } else if (result === 'O'){
       this.gameState.gameEnded = true;
       this.setState({
         winner : 'O',
         winnerLine: 'Match won by O'
       })
-    } else if(result =='draw'){
+    } else if(result ==='draw'){
       this.gameState.gameEnded = true;
       this.setState({
         winner: 'draw',
@@ -65,12 +65,12 @@ class App extends Component {
       [6,7,8]];
     var board = this.gameState.board;
     for(let i=0;i<moves.length;i++){
-      if(board[moves[i][0]] == board[moves[i][1]] && board[moves[i][1]] == board[moves[i][2]])
+      if(board[moves[i][0]] === board[moves[i][1]] && board[moves[i][1]] === board[moves[i][2]])
       return board[moves[i][0]];
     }
 
     console.log(this.gameState.totalMoves)
-    if(this.gameState.totalMoves == 9 ){
+    if(this.gameState.totalMoves === 9 ){
       return 'draw';
     }
   }
